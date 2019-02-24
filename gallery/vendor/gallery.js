5847bc0ef999c1e5ab17c1107fabf1e7d3ca6c2e
Plamen Ivanov - LT <plamko@abv.bg>
2019-02-24 22:31:41 +0200

    update site content - gallery

--- gallery/vendor/gallery.js
+++ gallery/vendor/gallery.js
@@ -1,43 +1,62 @@
 $(document).ready(function () {
   let productsLink = {
-    1: 'ups',
-    2: 'adaptori',
-    3: 'kolektori',
-    4: 'kotli',
-    5: 'kranove',
-    6: 'liri',
-    7: 'liri-hrom',
-    8: 'obezv',
-    9: 'podlojki',
-    10: 'pompi',
-    11: 'rad-al',
-    12: 'rad-pal',
-    13: 'razsh-sad',
-    14: 'trybi'
+    1: 'alum_rad',
+    2: 'panelni_rad',
+    3: 'liri',
+    4: 'auto_obezv',
+    5: 'adaptori',
+    6: 'garnituri',
+    7: 'kit',
+    8: 'kolektori_podovo',
+    9: 'kolektori',
+    10: 'podlojka',
+    11: 'pompi',
+    12: 'pres_adaptori',
+    13: 'rad_kranove',
+    14: 'raz_sudove',
+    15: 'kuki',
+    16: 'sfer_kranove',
+    17: 'termoglavi',
+    18: 'termostati',
+    19: 'trubi_pex',
+    20: 'trubi_pex_iz',
+    21: 'trubi_podovo',
+    22: 'c_pompi',
+    23: 'ups'
   }
   let productsBg = {
-    1: 'UPS',
-    2: 'адаптори',
-    3: 'колектори',
-    4: 'котли',
-    5: 'кранове',
-    6: 'лири',
-    7: 'лири хром',
-    8: 'обезвъздушители',
-    9: 'подложки',
-    10: 'помпи',
-    11: 'радиатори алуминиеви',
-    12: 'радиатори панелни',
-    13: 'разширителни съдове',
-    14: 'тръби'
+    1: 'алуминиеви радиатори',
+    2: 'панелни радиатори',
+    3: 'лири за баня',
+    4: 'автоматични обезвъздушители',
+    5: 'адаптори',
+    6: 'гарнитури за ал. радиатори 1,5мм',
+    7: 'кит комплекти',
+    8: 'колектори за подово',
+    9: 'месингови колектори',
+    10: 'подложка',
+    11: 'помпи',
+    12: 'прес адаптори',
+    13: 'радиаторни кранове',
+    14: 'разширителни съдове',
+    15: 'куки регулируеми',
+    16: 'сферични кранове',
+    17: 'термоглави',
+    18: 'термостати',
+    19: 'тръби pex',
+    20: 'тръби pex с изолация',
+    21: 'тръби за подово',
+    22: 'циркулационни помпи',
+    23: 'ups'
   }
   let productsSection = $('.row')
   let fragment = document.createDocumentFragment()
-  for (let i = 1; i <= 14; i++) {
-    if (i == 11) continue
-    let current = `<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+  for (let i = 1; i <= 23; i++) {
+    let current = `<div class="col-lg-3 col-md-4 col-sm-6 col-6">
     <a href="./img/${productsLink[i]}/" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="./img/${productsLink[i]}/thumbs/IMG1_tn.jpg" alt="">
+      <div class="img-wraper">
+        <img class="img-fluid img-thumbnail" src="./img/${productsLink[i]}/thumbs/IMG (1).jpg" alt="">
+      </div>
       <p>${productsBg[i].toUpperCase()}</p>
     </a>
   </div>`
